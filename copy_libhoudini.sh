458d8e2d8f399383892ec5f9b96da49ad723282e
Zhenghua Wang <zhenghua.wang@intel.com>
2013-07-24 11:53:57 +0800

    houdini: Release arm binary directly and fix errors report from Coverity Prevent tool
    BZ: 123479
    
    1. Remove houdini arm libraries build scripts and release arm binary directly
    2. Fix errors report from Coverity Prevent tool. (BZ123479)
    
    Change-Id: I91c0f6eedf1a0e4ea3301390b2e0636152640a31
    Signed-off-by: llin4 <ling.lin@intel.com>
    Signed-off-by: Zhenghua Wang <zhenghua.wang@intel.com>

--- copy_libhoudini.sh
+++ copy_libhoudini.sh
@@ -6,12 +6,14 @@ HOUDINI_VERSION=`strings ${HOUDINI_PATH}/system/lib/libhoudini.so | grep version
 PRODUCT_OUT=${TOP}/$2
 
 mkdir -p ${PRODUCT_OUT}/obj/SHARED_LIBRARIES/libhoudini_intermediates/LINKED/
+mkdir -p ${PRODUCT_OUT}/symbols/system/lib/
+mkdir -p ${PRODUCT_OUT}/system/lib/
 
 ### Copy libhoudini with version
 echo "Copying libhoudini"
-cp -f --remove-destination ${HOUDINI_PATH}/system/lib/libhoudini.so ${PRODUCT_OUT}/obj/SHARED_LIBRARIES/libhoudini_intermediates/LINKED/libhoudini.so.${HOUDINI_VERSION}
-cp -f --remove-destination ${HOUDINI_PATH}/system/lib/libhoudini.so ${PRODUCT_OUT}/symbols/system/lib/libhoudini.so.${HOUDINI_VERSION}
-cp -f --remove-destination ${HOUDINI_PATH}/system/lib/libhoudini.so ${PRODUCT_OUT}/system/lib/libhoudini.so.${HOUDINI_VERSION}
+cp -f ${HOUDINI_PATH}/system/lib/libhoudini.so ${PRODUCT_OUT}/obj/SHARED_LIBRARIES/libhoudini_intermediates/LINKED/libhoudini.so.${HOUDINI_VERSION}
+cp -f ${HOUDINI_PATH}/system/lib/libhoudini.so ${PRODUCT_OUT}/symbols/system/lib/libhoudini.so.${HOUDINI_VERSION}
+cp -f ${HOUDINI_PATH}/system/lib/libhoudini.so ${PRODUCT_OUT}/system/lib/libhoudini.so.${HOUDINI_VERSION}
 
 ### Create soft link for the libhoudini.so
 cd ${PRODUCT_OUT}/obj/SHARED_LIBRARIES/libhoudini_intermediates/LINKED
