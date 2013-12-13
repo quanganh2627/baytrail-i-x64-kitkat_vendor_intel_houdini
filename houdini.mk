3dadefb444459616fe2b538eb0239e91f97f8637
Russell Webb <russell.webb@intel.com>
2013-12-13 10:03:56 -0800

    Set ABI2 when Houdini is included
    This property must be set whenever houdini is enabled to inform
    the frameworks that we do support the arm-v7 instruction set.
    
    Change-Id: Ib1c752291588f24449dc30820ac4f0040337bc88

--- houdini.mk
+++ houdini.mk
@@ -109,4 +109,7 @@ PRODUCT_COPY_FILES += \
     ${HOUDINI_PATH}/system/lib/arm/cpuinfo:system/lib/arm/cpuinfo \
     ${HOUDINI_PATH}/system/lib/arm/cpuinfo.neon:system/lib/arm/cpuinfo.neon \
 
+#Set CPU ABI
+ADDITIONAL_BUILD_PROPERTIES += ro.product.cpu.abi2=armeabi-v7a
+
 endif
