359290d41aaf064a912f1209f83e93d47c26b09a
jgu21 <jinghui.gu@intel.com>
2014-12-31 03:36:46 -0500

    houdini: Houdini 5.0.1 release for Lollipop MR0
    This is the maintenance release for Lollipop MR0.
    
    1. Improve the support for devices without SSE4 ISA
    2. Improve the signal handling routines
    3. Improve the support for arm executables
    4. Fix issues duo to code changes in bionic
    5. Fix performance issues duo to kernel changes
    6. Fix Netflix hang issue tracked on IMINAN-11698
    7. Fix issues which caused app nfs13 crashed
    8. Fix issues which caused app watchABC crashed restarting from
       abnormal close, tracked on BZ228776
    9. Miscellaneous improvements for houdini stability
    
    Change-Id: I6fa80505dec1f4cd7846f09de8cc70ed08684d9f
    Tracked-On: https://jira01.devtools.intel.com/browse/IMINAN-15631
    Signed-off-by: jgu21 <jinghui.gu@intel.com>
    Reviewed-on: https://android.intel.com:443/315855

--- houdini.mk
+++ houdini.mk
@@ -4,184 +4,73 @@
 # which is subject to a non-disclosure agreement between Intel Corporation
 # and you or your company.
 
-# Native Bridge Build Flag
+# Enable Native Bridge Build Flag
 WITH_NATIVE_BRIDGE := true
 
-# Installing Native Bridge
+# Install Native Bridge
 ifeq ($(WITH_NATIVE_BRIDGE),true)
 
-# Native Bridge Lib Name
-NATIVE_BRIDGE_LIB_NAME := libhoudini.so
+# Native Bridge Binary Name
+NB_EXE_NAME := houdini
+NB_EXE64_NAME := houdini64
+NB_LIB_NAME := libhoudini.so
 
-# Native Bridge Executable Name
-NATIVE_BRIDGE_EXECUTABLE_NAME := houdini
+NB_PATH := vendor/intel/houdini
+NB_LIB_PATH := system/lib
+NB_ARM_PATH := $(NB_LIB_PATH)/arm
+NB_NBLIB_PATH := $(NB_ARM_PATH)/nb
+NB_LIB64_PATH := system/lib64
+NB_ARM64_PATH := $(NB_LIB64_PATH)/arm64
+NB_NBLIB64_PATH := $(NB_ARM64_PATH)/nb
+NB_BIN_PATH := system/bin
+NB_FMT_PATH := system/etc/binfmt_misc
 
-# Native Bridge Path
-NATIVE_BRIDGE_PATH := vendor/intel/houdini
-
-# Native Bridge Registration Path
-NATIVE_BRIDGE_BINFMT_MISC_PATH := $(NATIVE_BRIDGE_PATH)/system/etc/binfmt_misc
-
-# Native Bridge Bin Path
-NATIVE_BRIDGE_BIN_PATH := $(NATIVE_BRIDGE_PATH)/system/bin
-
-# Native Bridge Lib Path
-NATIVE_BRIDGE_LIB_PATH := $(NATIVE_BRIDGE_PATH)/system/lib
-
-# Native Bridge Target Lib Path
-NATIVE_BRIDGE_TARGET_LIB_PATH := $(NATIVE_BRIDGE_LIB_PATH)/arm
-
-# Native Bridge NB Lib Path
-NATIVE_BRIDGE_NB_LIB_PATH := $(NATIVE_BRIDGE_TARGET_LIB_PATH)/nb
-
-# Native Bridge Target Image Lib Path
-NATIVE_BRIDGE_TARGET_IMG_LIB_PATH := /system/lib/arm
-
-# Native Bridge NB Image Lib Path
-NATIVE_BRIDGE_IMG_NB_LIB_PATH := $(NATIVE_BRIDGE_TARGET_IMG_LIB_PATH)/nb
-
-# Native Bridge 64 Bit Lib Path
-NATIVE_BRIDGE_LIB64_PATH := $(NATIVE_BRIDGE_PATH)/system/lib64
-
-# Native Bridge Target 64 Bit Lib Path
-NATIVE_BRIDGE_TARGET_LIB64_PATH := $(NATIVE_BRIDGE_LIB64_PATH)/arm64
-
-# Native Bridge 64 Bit NB Lib Path
-NATIVE_BRIDGE_NB_LIB64_PATH := $(NATIVE_BRIDGE_TARGET_LIB64_PATH)/nb
-
-# Native Bridge Target Image 64 Bit Lib Path
-NATIVE_BRIDGE_TARGET_IMG_LIB64_PATH := /system/lib64/arm64
-
-# Native Bridge NB Image 64 Bit Lib Path
-NATIVE_BRIDGE_IMG_NB_LIB64_PATH := $(NATIVE_BRIDGE_TARGET_IMG_LIB64_PATH)/nb
-
-# Native Bridge ABI List
-NATIVE_BRIDGE_ABI_LIST_32_BIT := armeabi
-
-# Native Bridge ABI 64 Bit List
-NATIVE_BRIDGE_ABI_LIST_64_BIT :=
-
-# Native Bridge for 64 Bit Kernel
-NATIVE_BRIDGE_64_BIT_KERNEL := false
+NB_32BIT_ENABLE := false
+NB_64BIT_ENABLE := false
+NB_32BIT_SUFFIX := x
 
 # Support 64 Bit Apps
 ifeq ($(TARGET_SUPPORTS_64_BIT_APPS),true)
-
-  # Set Native Bridge with 64 Bit Kernel
-  NATIVE_BRIDGE_64_BIT_KERNEL := true
-
-  # Set 64 Bit ABI List
-  TARGET_CPU_ABI ?= x86_64
-  TARGET_CPU_ABI2 ?= arm64-v8a
-  TARGET_CPU_ABI_LIST_64_BIT := $(TARGET_CPU_ABI) $(TARGET_CPU_ABI2) $(NATIVE_BRIDGE_ABI_LIST_64_BIT)
-
-  # Copying Native Bridge 64 Bit Lib
-  # PRODUCT_COPY_FILES += $(NATIVE_BRIDGE_LIB64_PATH)/libhoudini_z.so:/system/lib64/$(NATIVE_BRIDGE_LIB_NAME):intel
-
-  # Copying Native Bridge Target 64 Bit Libs
-  # PRODUCT_COPY_FILES += $(foreach LIB64, $(filter-out nb, $(notdir $(wildcard $(NATIVE_BRIDGE_TARGET_LIB64_PATH)/*))), \
-      $(NATIVE_BRIDGE_TARGET_LIB64_PATH)/$(LIB64):$(NATIVE_BRIDGE_TARGET_IMG_LIB64_PATH)/$(LIB64):intel)
-
-  # Copying Native Bridge NB 64 Bit Libs
-  # PRODUCT_COPY_FILES += $(foreach NB64, $(notdir $(wildcard $(NATIVE_BRIDGE_NB_LIB64_PATH)/*)), \
-      $(NATIVE_BRIDGE_NB_LIB64_PATH)/$(NB64):$(NATIVE_BRIDGE_IMG_NB_LIB64_PATH)/$(NB64):intel)
-
-  # Reset Dex Code Instruction Set
-  ADDITIONAL_BUILD_PROPERTIES += ro.dalvik.vm.isa.arm64=x86_64
-
-  # Also Support 32 Bit Apps
+  NB_64BIT_ENABLE = true
   ifeq ($(TARGET_SUPPORTS_32_BIT_APPS),true)
-
-    # Set 32 Bit ABI List
-    TARGET_2ND_CPU_ABI ?= x86
-    TARGET_2ND_CPU_ABI2 ?= armeabi-v7a
-    TARGET_CPU_ABI_LIST_32_BIT := $(TARGET_2ND_CPU_ABI) $(TARGET_2ND_CPU_ABI2) $(NATIVE_BRIDGE_ABI_LIST_32_BIT)
-
-    # Copying Native Bridge Lib
-    PRODUCT_COPY_FILES += $(NATIVE_BRIDGE_LIB_PATH)/libhoudini_y.so:/system/lib/$(NATIVE_BRIDGE_LIB_NAME):intel
-
-    # Copying Native Bridge Target Libs
-    PRODUCT_COPY_FILES += $(foreach LIB, $(filter-out nb, $(notdir $(wildcard $(NATIVE_BRIDGE_TARGET_LIB_PATH)/*))), \
-        $(NATIVE_BRIDGE_TARGET_LIB_PATH)/$(LIB):$(NATIVE_BRIDGE_TARGET_IMG_LIB_PATH)/$(LIB):intel)
-
-    # Copying Native Bridge NB Libs
-    PRODUCT_COPY_FILES += $(foreach NB, $(notdir $(wildcard $(NATIVE_BRIDGE_NB_LIB_PATH)/*)), \
-        $(NATIVE_BRIDGE_NB_LIB_PATH)/$(NB):$(NATIVE_BRIDGE_IMG_NB_LIB_PATH)/$(NB):intel)
-
-    # Reset Dex Code Instruction Set
-    ADDITIONAL_BUILD_PROPERTIES += ro.dalvik.vm.isa.arm=x86
-
-  # End of TARGET_SUPPORTS_32_BIT_APPS
+    NB_32BIT_ENABLE = true
+    NB_32BIT_SUFFIX = y
   endif
-
-# Support 32 Bit Apps Only
 else
-
-  # Set Native Bridge with 64 Bit Kernel
+  NB_32BIT_ENABLE = true
   ifeq ($(TARGET_KERNEL_ARCH),x86_64)
-    NATIVE_BRIDGE_64_BIT_KERNEL := true
+    NB_32BIT_SUFFIX = y
   else
-    NATIVE_BRIDGE_64_BIT_KERNEL := $(BOARD_USE_64BIT_KERNEL)
+    ifeq ($(BOARD_USE_64BIT_KERNEL),true)
+      NB_32BIT_SUFFIX = y
+    endif
   endif
-
-  # Set 32 Bit ABI List
-  TARGET_CPU_ABI ?= x86
-  TARGET_CPU_ABI2 ?= armeabi-v7a
-  TARGET_CPU_ABI_LIST_32_BIT := $(TARGET_CPU_ABI) $(TARGET_CPU_ABI2) $(NATIVE_BRIDGE_ABI_LIST_32_BIT)
-
-  # Supoort 64 Bit Kernel
-  ifeq ($(NATIVE_BRIDGE_64_BIT_KERNEL),true)
-
-    # Copying Native Bridge 64 Bit Lib
-    PRODUCT_COPY_FILES += $(NATIVE_BRIDGE_LIB_PATH)/libhoudini_y.so:/system/lib/$(NATIVE_BRIDGE_LIB_NAME):intel
-
-  # Supoort 32 Bit Kernel
-  else
-
-    # Copying Native Bridge 32 Bit Lib
-    PRODUCT_COPY_FILES += $(NATIVE_BRIDGE_LIB_PATH)/libhoudini_x.so:/system/lib/$(NATIVE_BRIDGE_LIB_NAME):intel
-
-  # End of NATIVE_BRIDGE_64_BIT_KERNEL
-  endif
-
-  # Copying Native Bridge Target Libs
-  PRODUCT_COPY_FILES += $(foreach LIB, $(filter-out nb, $(notdir $(wildcard $(NATIVE_BRIDGE_TARGET_LIB_PATH)/*))), \
-      $(NATIVE_BRIDGE_TARGET_LIB_PATH)/$(LIB):$(NATIVE_BRIDGE_TARGET_IMG_LIB_PATH)/$(LIB):intel)
-
-  # Copying Native Bridge NB Libs
-  PRODUCT_COPY_FILES += $(foreach NB, $(notdir $(wildcard $(NATIVE_BRIDGE_NB_LIB_PATH)/*)), \
-      $(NATIVE_BRIDGE_NB_LIB_PATH)/$(NB):$(NATIVE_BRIDGE_IMG_NB_LIB_PATH)/$(NB):intel)
-
-  # Reset Dex Code Instruction Set
-  ADDITIONAL_BUILD_PROPERTIES += ro.dalvik.vm.isa.arm=x86
-
-# End of TARGET_SUPPORTS_64_BIT_APPS
 endif
 
-# Support 64 Bit Kernel
-ifeq ($(NATIVE_BRIDGE_64_BIT_KERNEL),true)
-
-  # Copying Native Bridge 64 Bit Executables
-  PRODUCT_COPY_FILES += $(NATIVE_BRIDGE_BIN_PATH)/houdini_y:/system/bin/$(NATIVE_BRIDGE_EXECUTABLE_NAME):intel
-
-# Support 32 Bit Kernel
-else
-
-  # Copying Native Bridge 32 Bit Executables
-  PRODUCT_COPY_FILES += $(NATIVE_BRIDGE_BIN_PATH)/houdini_x:/system/bin/$(NATIVE_BRIDGE_EXECUTABLE_NAME):intel
-
-# End of NATIVE_BRIDGE_64_BIT_KERNEL
+ifeq ($(NB_64BIT_ENABLE),disabled)
+  PRODUCT_COPY_FILES += $(NB_PATH)/$(NB_BIN_PATH)/houdini_z:$(NB_BIN_PATH)/$(NB_EXE64_NAME):intel
+  PRODUCT_COPY_FILES += $(NB_PATH)/$(NB_LIB64_PATH)/libhoudini_z.so:$(NB_LIB64_PATH)/$(NB_LIB_NAME):intel
+  PRODUCT_COPY_FILES += $(foreach LIB64, $(filter-out nb, $(notdir $(wildcard $(NB_PATH)/$(NB_ARM64_PATH)/*))), \
+      $(NB_PATH)/$(NB_ARM64_PATH)/$(LIB64):$(NB_ARM64_PATH)/$(LIB64):intel)
+  PRODUCT_COPY_FILES += $(foreach NB64, $(notdir $(wildcard $(NB_PATH)/$(NB_NBLIB64_PATH)/*)), \
+      $(NB_PATH)/$(NB_NBLIB64_PATH)/$(NB64):$(NB_NBLIB64_PATH)/$(NB64):intel)
+  PRODUCT_COPY_FILES += $(foreach FMT64, $(filter arm64_%, $(notdir $(wildcard $(NB_PATH)/$(NB_FMT_PATH)/*))), \
+      $(NB_PATH)/$(NB_FMT_PATH)/$(FMT64):$(NB_FMT_PATH)/$(FMT64):intel)
+  ADDITIONAL_BUILD_PROPERTIES += ro.dalvik.vm.isa.arm64=x86_64 ro.enable.native.bridge.exec64=1
 endif
 
-# Copying BINFMT_MISC Interpreter Registration Files
-PRODUCT_COPY_FILES += $(foreach FMT, $(notdir $(wildcard $(NATIVE_BRIDGE_BINFMT_MISC_PATH)/*)), \
-    $(NATIVE_BRIDGE_BINFMT_MISC_PATH)/$(FMT):/system/etc/binfmt_misc/$(FMT):intel)
+ifeq ($(NB_32BIT_ENABLE),true)
+  PRODUCT_COPY_FILES += $(NB_PATH)/$(NB_BIN_PATH)/houdini_$(NB_32BIT_SUFFIX):$(NB_BIN_PATH)/$(NB_EXE_NAME):intel
+  PRODUCT_COPY_FILES += $(NB_PATH)/$(NB_LIB_PATH)/libhoudini_$(NB_32BIT_SUFFIX).so:$(NB_LIB_PATH)/$(NB_LIB_NAME):intel
+  PRODUCT_COPY_FILES += $(foreach LIB, $(filter-out nb, $(notdir $(wildcard $(NB_PATH)/$(NB_ARM_PATH)/*))), \
+      $(NB_PATH)/$(NB_ARM_PATH)/$(LIB):$(NB_ARM_PATH)/$(LIB):intel)
+  PRODUCT_COPY_FILES += $(foreach NB, $(notdir $(wildcard $(NB_PATH)/$(NB_NBLIB_PATH)/*)), \
+      $(NB_PATH)/$(NB_NBLIB_PATH)/$(NB):$(NB_NBLIB_PATH)/$(NB):intel)
+  PRODUCT_COPY_FILES += $(foreach FMT, $(filter arm_%, $(notdir $(wildcard $(NB_PATH)/$(NB_FMT_PATH)/*))), \
+      $(NB_PATH)/$(NB_FMT_PATH)/$(FMT):$(NB_FMT_PATH)/$(FMT):intel)
+  ADDITIONAL_BUILD_PROPERTIES += ro.dalvik.vm.isa.arm=x86 ro.enable.native.bridge.exec=1
+endif
 
-# Enable Native Bridge Executable by default
-ADDITIONAL_BUILD_PROPERTIES += ro.enable.native.bridge.exec=1
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.dalvik.vm.native.bridge=$(NB_LIB_NAME)
 
-# Native Bridge Lib Name
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.dalvik.vm.native.bridge=$(NATIVE_BRIDGE_LIB_NAME)
-
-# End of WITH_NATIVE_BRIDGE
 endif
